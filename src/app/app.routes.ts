667c0cd4881f0486e1cdd3ea033a9a9bc68832f3
kike <egarciavicent@gmail.com>
2025-03-29 15:19:31 +0100

    feat: añadido componentes y rutas para inicio, carta, eventos, reservas, contacto, locales y cuenta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CartaComponent } from './pages/carta/carta.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
+import { ReservasComponent } from './pages/reservas/reservas.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { LocalesComponent } from './pages/locales/locales.component';
+import { CuentaComponent } from './pages/cuenta/cuenta.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'carta', component: CartaComponent },
+  { path: 'eventos', component: EventosComponent },
+  { path: 'reservas', component: ReservasComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'locales', component: LocalesComponent },
+  { path: 'cuenta', component: CuentaComponent },
+  { path: '', component: InicioComponent },
+  { path: '**', redirectTo: '/carta' },
+];
