b98bd9e74e7fc8ad5d5fd41d136534fb98d28f20
kike <egarciavicent@gmail.com>
2025-03-29 16:30:03 +0100

    feat: añadido componente footer y actualizado app.component para incluirlo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HeaderComponent } from './components/header/header.component'; 
-
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
