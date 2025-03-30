e6482fd8a358825a34a99fcc129662d5e47962bd
kike <egarciavicent@gmail.com>
2025-03-30 10:37:57 +0200

    feat: prueba de intregacion con API simulada para cargar imagenes

---
+++ src/app/components/galeria/galeria.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-galeria',
+  imports: [CommonModule],
+  templateUrl: './galeria.component.html',
+  styleUrl: './galeria.component.css'
+})
+export class GaleriaComponent implements OnInit {
+  fotos: { url: string }[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<{ url: string }[]>('/assets/api/instagram.json').subscribe((data) => {
+      this.fotos = data;
+    });
+  }
+}
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { GaleriaComponent } from '../../components/galeria/galeria.component';
+import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule, GaleriaComponent],
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
