bbd8649ca583a69ace04325ed8dae17e39da05b7
kike <egarciavicent@gmail.com>
2025-04-20 14:59:45 +0200

    feat: añadido formsModule y ngModel para menejar el login del administrador

--- src/app/pages/admin-login/admin-login.component.ts
+++ src/app/pages/admin-login/admin-login.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   standalone: true,
   selector: 'app-admin-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-login.component.html',
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent {
+  usuario = '';
+  contrasena = '';
+  
   cerrarModal() {
     window.location.href = '/';
+  }
+  iniciarSesion() {
+    console.log('Usuario:', this.usuario);
+    console.log('Contraseña:', this.contrasena);
   }
 }
