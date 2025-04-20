b03eea5b17caee7711bb5c4fa1265ac78f0ca29c
kike <egarciavicent@gmail.com>
2025-04-20 15:20:24 +0200

    feat: añadido Router para probar e login del administrador

--- src/app/pages/admin-login/admin-login.component.ts
+++ src/app/pages/admin-login/admin-login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,12 +13,22 @@ import { FormsModule } from '@angular/forms';
 export class AdminLoginComponent {
   usuario = '';
   contrasena = '';
+
+  constructor(private router: Router) {}
   
   cerrarModal() {
     window.location.href = '/';
   }
   iniciarSesion() {
-    console.log('Usuario:', this.usuario);
-    console.log('Contraseña:', this.contrasena);
+    const usuarioValido = 'admin';
+    const contrasenaValida = 'admin123';
+    if (this.usuario === usuarioValido && this.contrasena === contrasenaValida) {
+
+      localStorage.setItem('admin', 'true');
+
+      this.router.navigate(['/admin/panel']);
+    }else {
+      alert('Usuario o contraseña incorrectos');
+    }
   }
 }
