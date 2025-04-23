49bb814b0b3d99f1178aec2b2ba96ff5c1e1602b
kike <egarciavicent@gmail.com>
2025-04-23 19:11:22 +0200

    feat: añadido panel de administrador para insertar productos en la bd y se muestran en la vista de la carta

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
+  standalone: true,
   selector: 'app-admin-panel',
-  imports: [],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.css'
 })
-export class AdminPanelComponent {
 
+export class AdminPanelComponent {
+  nombre = '';
+  precio: number | null = null;
+  categoria_id: number | null = null;
+  categorias: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3000/categorias').subscribe((data) => {
+      this.categorias = data;
+    });
+  }
+
+  enviarProducto() {
+    const nuevoProducto = {
+      nombre: this.nombre,
+      precio: this.precio,
+      categoria_id: this.categoria_id
+    };
+
+    this.http.post('http://localhost:3000/productos', nuevoProducto).subscribe(
+      (res) => {
+       alert('Producto añadido correctamente ');
+        this.nombre = '';
+        this.precio = null;
+        this.categoria_id = null;
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
 }
